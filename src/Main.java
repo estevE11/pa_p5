8a0430ef0155e2b80af02c62b763fb5dc2c3226b
Roger Esteve <rogeresteve30@gmail.com>
2022-12-03 16:51:24 +0100

    test de q funciona

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,15 @@ import Producte.Producte;
 
 public class Main {
     public static void main(String[] args) {
-        Producte[] productes = new Producte[5];
+        Producte[] productes = new Producte[7];
         int[][] reaccions = {
-                new int[] { 3, 4, 5 },
-                new int[] { 3, 4 },
-                new int[] { 1, 2 },
-                new int[] { 1, 5, 2 },
-                new int[] { 1, 4 }
+                new int[] { 2, 4, 5 }, // 1
+                new int[] { 1, 3, 4 }, // 2
+                new int[] { 1, 2, 7 }, // 3
+                new int[] { 3, 5, 2, 6, 7}, // 4
+                new int[] { 1, 4 }, // 5
+                new int[] { 7, 4 }, // 6
+                new int[] { 4, 6, 3 } // 7
         };
 
         for (int i = 0; i < productes.length; i++) {
